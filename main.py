fc949aff2a0dcc27bc60a6c468b0ffc18ae85926
Solene <solene.patruno@smile.fr>
2019-04-23 13:48:11 +0200

    try

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 #!/usr/local/bin/python3.7
 def hi_there():
       usr_name = input('Comment vous appelez vous ? ')
-      print ('Bonjour, je suis le chatbot Healpy, que puis-je faire pour vous aider ?')
-      usr_ans = input()
-      print ('Bonjour à vous,', usr_name)
+      usr_ans = input('Bonjour à vous, je suis le chatbot Healpy, enchanté de vous connaître, que puis-je faire pour vous servir ?')
+      print ('Je vais vous aider', usr_name)
 
 hi_there()
