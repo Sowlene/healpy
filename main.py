85a28d159498105d287e22b33bae27506472568e
Solene <solene.patruno@smile.fr>
2019-04-23 13:26:12 +0200

    go into console chatbot

---
+++ main.py
@@ -0,0 +1,8 @@
+#!/usr/local/bin/python3.7
+def hi_there():
+      usr_name = input('Comment vous appelez vous ? ')
+      print ('Bonjour, je suis le chatbot Healpy, que puis-je faire pour vous aider ?')
+      usr_ans = input()
+      print ('Bonjour à vous,', usr_name)
+
+hi_there()
